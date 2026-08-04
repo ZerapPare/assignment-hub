485b617faa7817e5997b74333fbade3016206fa5
satangKung <paphawit145@gmail.com>
2026-08-04 11:17:11 +0700

    add: migrate script

---
+++ migrate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Running migrations..."
+docker compose exec -T db mysql -uroot -proot123 assignment_hub < migrations/001_identity.sql
+docker compose exec -T db mysql -uroot -proot123 assignment_hub < migrations/002_task_type.sql
+docker compose exec -T db mysql -uroot -proot123 assignment_hub < migrations/003_status_updated_at.sql
+echo "Migrations complete!"
