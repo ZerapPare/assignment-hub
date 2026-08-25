2842d1bfd2ccdb5411847b2913b47559aa1aa67a
ParamaWattanakrai <Parama.Wattanakrai@gmail.com>
2026-08-25 18:36:41 +0700

    announcements fix

--- migrate.bat
+++ migrate.bat
@@ -5,4 +5,5 @@ docker compose exec -T db mysql -uroot -proot123 assignment_hub < migrations/002
 docker compose exec -T db mysql -uroot -proot123 assignment_hub < migrations/003_status_updated_at.sql
 docker compose exec -T db mysql -uroot -proot123 assignment_hub < migrations/004_notification_settings.sql
 docker compose exec -T db mysql -uroot -proot123 assignment_hub < migrations/005_admin_monitoring.sql
+docker compose exec -T db mysql -uroot -proot123 assignment_hub < migrations/006_announcement.sql
 echo Migrations complete!
