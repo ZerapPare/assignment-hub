af8bbf6a85d40d4a60771a71648a10fb359d8a49
ParamaWattanakrai <Parama.Wattanakrai@gmail.com>
2026-08-25 18:55:27 +0700

    Assignment Detail

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import GlobalStyles from './GlobalStyles';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
 import AssignmentsPage from './pages/AssignmentsPage';
+import AssignmentDetailPage from './pages/AssignmentDetailPage';
 import StreamPage from './pages/StreamPage';
 import SettingsPage from './pages/SettingsPage';
 import AdminLayout from './components/admin/AdminLayout';
@@ -21,6 +22,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/assignments" element={<AssignmentsPage />} />
+        <Route path="/assignments/:id" element={<AssignmentDetailPage />} />
         <Route path="/stream" element={<StreamPage />} />
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/admin" element={<AdminLayout />}>
